fe82d5ea3315bb2af733b48965dc0affb79c6785
Димка Стиклов <dimkastiklov@Dimkas-MacBook-Pro.local>
2024-01-24 02:59:27 +0400

    fix: add windows OS support for 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,7 +6,7 @@ fs.readdir(folder, {withFileTypes: true}, (err, files) => {
     if (err) console.error(err);
     for (const file of files) {
         if (file.isFile()) {
-            const filePath = path.join(folder, file.name);
+            const filePath = path.join(folder, file.name).replace(/\\/g, '/');
             const name = filePath.split('/').reverse()[0].split('.')[0];
             const extension = filePath.split('.')[1];
             if (extension === 'DS_Store') continue;
